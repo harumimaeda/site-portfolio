837b963d72cb4ab9c2d07e30e0ef3b6d0bb281c5
Harumi Maeda <harumiumaeda@gmail.com>
2021-07-07 01:13:54 -0300

    Alterada a vitrine (sujeita à mudanças), adicionada a animação do texto e os cards para os trabalhos).

---
+++ js/animations.js
@@ -0,0 +1,12 @@
+function typewriter(element) {
+  
+    let letters = element.innerHTML.split('');
+    element.innerHTML = ''; 
+    
+    letters.forEach(( letter, index, letters ) => {
+      setTimeout(() => element.innerHTML += letter, 100 * index);
+    });
+  }
+  
+  const text = document.querySelector('.typewriter');
+  typewriter(text);
